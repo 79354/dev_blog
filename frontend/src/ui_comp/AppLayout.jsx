689039fb5cce870bd98b0e68d52f5cd708c483b3
79354 <gillramansingh900@gmail.com>
2025-11-14 17:51:59 +0530

    create homepage

--- frontend/src/ui_comp/AppLayout.jsx
+++ frontend/src/ui_comp/AppLayout.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./Footer"
 import NavBar from "./NavBar"
 
@@ -21,9 +22,21 @@ const AppLayout = ({ isAuthenticated, username, setIsAuthenticated, setUsername 
 
     return (
         <div className={darkMode ? "dark" : ""}>
+            <main className="w-full bg-[#ffffff] dark:bg-[#181A2A]">
+                <NavBar
+                    darkMode={darkMode}
+                    handleDarkMode={handleDarkMode}
+                    isAuthenticated={isAuthenticated}
+                    username = {username}
+                    setIsAuthenticated = { setIsAuthenticated }
+                    setUsername = { setUsername }
+                />
+                <ToastContainer/>
+                <Outlet/>
+                <Footer/>
+            </main>
         </div>
-    )
-
-}
+    );
+};
 
 export default AppLayout;
