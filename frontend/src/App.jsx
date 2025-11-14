3f35c3a2a48ea66bdbd3da913882e93a46444fab
79354 <gillramansingh900@gmail.com>
2025-11-14 17:08:58 +0530

    added navbar and responsive navbar for mobile users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,2 +1,40 @@
 import { BrowserRouter, Routes, Router } from "react-router-dom";
 import AppLayout from "./ui_comp/AppLayout.jsx"
+import { Button } from "./components/ui/button.jsx";
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const App = () => {
+    return (
+        <div>
+            const [username, setUsername] = useState(null);
+            const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+            const { data } = useQuery({
+                queryKey: ["username"],
+                queryFn: getUsername,
+            });
+
+            useEffect(
+                function () {
+                if (data) {
+                    setUsername(data.username);
+                    setIsAuthenticated(true);
+                }
+                },
+                [data]
+            );
+
+            return (
+                <BrowserRouter>
+                    <Routes>
+
+                    </Routes>
+                </BrowserRouter>
+            )
+
+        </div>
+    )
+}
+
+export default App;
---
+++ frontend/src/ui_comp/NavBar.jsx
@@ -0,0 +1,103 @@
+import { Switch } from "@radix-ui/react-switch";
+import { FaHamburger } from "react-icons/fa";
+import ResponsiveNavBar from "./ResponsiveNavBar";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = ({
+    darkMode,
+    handleDarkMode,
+    isAuthenticated,
+    username,
+    setIsAuthenticated,
+    setUsername,
+}) => {
+    const [showNavBar, setShowNavBar] = useState(false);
+
+    function logout() {
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        setIsAuthenticated(false);
+        setUsername(null);
+    }
+
+    return (
+        <>
+            <nav className="max-container padding-x py-6 flex justify-center items-center gap-6 sticky top-0 z-10  bg-[#FFFFFF] dark:bg[#141624]">
+                <Link to="/" className="text-[#141624] text-2xl dark:text-[#FFFFFF]">
+                    DevBlog
+                </Link>
+                <ul className="flex items-center justify-end gap-9 text-[#3B3C4A] lg:flex-1 max-md:hidden dark:text-[#FFFFFF]">
+                    {isAuthenticated ? (
+                        <>
+                            <li><NavLink
+                                to={`/profile/${username}`}
+                                className={({isActive}) => (isActive ? "active" : "")}
+                            >
+                                Hi, {username}
+                            </NavLink></li>
+
+                            <li onClick={logout} className="cursor-pointer">
+                                logout
+                            </li>
+                        </>
+
+                    ) : (
+                        <>
+                            <li>
+                                <NavLink
+                                    to="/signin"
+                                    className={({isActive}) => (isActive ? "active" : "")}
+                                >
+                                    Login
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink
+                                    to="/signup"
+                                    className={({isActive}) => (isActive ? "active" : "")}
+                                >
+                                    Register
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+
+                    <li className="font-semibold">
+                        <NavLink
+                            to="/create"
+                            className={({isActive}) => (isActive ? "active" : "")}
+                        >
+                            Create Post
+                        </NavLink>
+
+                    </li>
+                </ul>
+
+                <Switch onCheckedChange={handleDarkMode} checked={darkMode} />
+                <FaHamburger 
+                    className="text-2xl cursor-pointer hidden max-md:block dark:text-white"
+                    onClick={() => setShowNavBar((curr) => !curr)}
+                />
+            </nav>
+
+            {showNavBar && (
+                <ResponsiveNavBar 
+                    isAuthenticated={isAuthenticated}
+                    username={username}
+                    logout={logout}
+                />
+            )}
+        </>
+    );
+};
+
+export default NavBar
+
+/*
+When the user logs out:
+    Tokens are removed
+    Auth state is reset
+    Username is cleared
+*/
